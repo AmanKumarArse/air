4875d208d5b2ce80a3d719727fb9790dd005958c
AmanKumarArse <amankumararse189@gmail.com>
2023-03-18 13:30:44 +0530

    first commit

---
+++ air.js
@@ -0,0 +1,35 @@
+gsap.from("#nav", {
+
+    duration: 1, 
+    y: -150, 
+    opacity:0,
+    
+  });
+  gsap.from("#bigtext",{
+    x:-1500,
+    duration:2,
+    opacity:0,
+
+  })
+gsap.from("#shoes",{
+    x:1500,
+    duration:2,
+    opacity:1,
+
+})
+gsap.from("#text", {
+
+    duration: 2, 
+    y: 1500, 
+    opacity:0,
+    
+  });
+
+
+  gsap.from("#red", {
+
+    duration: 2, 
+    y: -1500, 
+    opacity:0,
+    
+  });
